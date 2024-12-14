32d03f0e56ffc4385cd19914749e1f210f7fa510
Aman Shygaibaev <aman.shygaibaev@gmail.com>
2024-12-14 15:51:36 -0600

    connection to database was added

---
+++ database_connection.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+        dbname = "projects",
+        user = "aman_m",
+        password = "aman20015",
+        host = "127.0.0.1",
+        port = "5432"
+    )
+
+except psycopg2.Error:
+    print("No connection to database")
+
+else:
+    cursor = conn.cursor()
+    
--- scrapeData.py
+++ scrapeData.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from database_connection import cursor
 
 targettedData = []
 amountOfPages = 50
