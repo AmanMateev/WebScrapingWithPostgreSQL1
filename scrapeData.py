2b1e1bc5d3d2cd506e4be795776c78ea40bf81bd
Aman Shygaibaev <aman.shygaibaev@gmail.com>
2025-01-03 16:17:59 -0600

    intermediate stage

--- scrapeData.py
+++ scrapeData.py
@@ -4,49 +4,77 @@ from database_connection import cursor
 from database_connection import conn
 
 
-#Получение чистого текста, без тегов html, в который они обернуты. BS возвращает текст с тегами, в которые они обернуты
-def clean_results(list):
-    cleaned_list = []
-    for listItem in list:
-        cleaned_list.append(listItem.text)
-    return cleaned_list
+
 #Функция, которая преобразует значения In stock в булевое значение (мне не нравится название переменной)
-def toBool(checkAvailability):
-    if checkAvailability == "In stock":
-        return True
+def isInStock(checkAvailability):
+    return checkAvailability == "In stock"
+     
+
+def formLinkToPage(index,genre):
+    genre_link = "https://books.toscrape.com/catalogue/category/books/"
+    if " " in genre:
+        splitted_genre = "-".join(genre.lower().split())
+        genre_link = genre_link + splitted_genre + f"_{index}" + "/index.html"
+        return genre_link
     else:
-        return False
+        genre_link = genre_link + genre.lower() + f'_{index}' + "/index.html"
+        return genre_link
+
 
 def dataExtractionURL(genrePageLink):
-    bookInfo1 = []
-    genrePageHtml = requests.get(genrePageLink)
-    if genrePageHtml.status_code !=200:
-        print(f"failed to load page: {genrePageHtml.status_code}")
-        return []
-    else:
-        soup = BeautifulSoup(genrePageHtml.text,'lxml')
-        price = soup.find_all('p', class_= "price_color")
-        availability = soup.find_all('p',class_ = "instock availability")
-        title = soup.select('h3 a')
-        genre = soup.select('.page-header h1')
-        for itemPrice, itemAvailability,title1 in zip(price,availability,title):
-            bufferTuple = (title1.text,float(itemPrice.text.lstrip("Â£")),toBool(itemAvailability.text.strip()),genre[0].text)
-            bookInfo1.append(bufferTuple)
-        return bookInfo1
+    try:
+        bookInfo1 = []
+        genrePageHtml = requests.get(genrePageLink)
+        if genrePageHtml.status_code !=200:
+            print(f"failed to load page: {genrePageHtml.status_code}")
+            return []
+        else:
+            soup = BeautifulSoup(genrePageHtml.text,'lxml')
+            price = soup.find_all('p', class_= "price_color")
+            availability = soup.find_all('p',class_ = "instock availability")
+            title = soup.select('h3 a')
+            genre = soup.select('.page-header h1')
+            if not(price and availability and title and genre):
+                print("Missign elements on the page")
+                return []
+            for itemPrice, itemAvailability,title1 in zip(price,availability,title):
+                bufferTuple = (title1.text,
+                               float(itemPrice.text.lstrip("Â£")),
+                               isInStock(itemAvailability.text.strip()),
+                               genre[0].text)
+                bookInfo1.append(bufferTuple)
+            return bookInfo1
+    except Exception as e:
+        print(f'Error extracting data from URL{e}')
+
+def validateBookData(book):
+    title, price, availability, genre = book
+    return (
+        isinstance(title, str) and
+        isinstance(price, float) and
+        isinstance(availability, bool) and
+        isinstance(genre, str)
+    )  
         
-def storeBooks(booksInfo1):
+def storeBooks(bookData):
+    validBooks = [book for book in bookData if validateBookData(book)]
+    if not validBooks:
+        print("No valid data to insert.")
+        return
+
     queryToDB = """INSERT INTO scrapped_books(title,price,availability,genre)
                    VALUES(%s,%s,%s,%s);
                 """
     try:
-        cursor.executemany(queryToDB,booksInfo1)
+        cursor.executemany(queryToDB,validBooks)
         print("Data added successfully!")
     except Exception as e:
         print(f"Error during executemany: {e}")
         conn.rollback()
     else:
         conn.commit()
-       
+
+
      
 
 
@@ -66,24 +94,16 @@ for genre in categories:
 
 #Формирование ссылок на страницы книг по жанрам, передача их в функцию для изъятия данных со страниц
 for index, genre in enumerate(formattedCategories, start= 2):
-    genre_link = "https://books.toscrape.com/catalogue/category/books/"
-    if " " in genre:
-        splitted_genre = "-".join(genre.lower().split())
-        genre_link = genre_link + splitted_genre + f"_{index}" + "/index.html"
+        genre_link = formLinkToPage(index,genre)
         bookInfo = dataExtractionURL(genre_link)
-        storeBooks(bookInfo)
-
-    else:
-        genre_link = genre_link + genre.lower() + f'_{index}' + "/index.html"
-        bookInfo = dataExtractionURL(genre_link)
-        storeBooks(bookInfo)
-
-        
-#Мне не нравится, что используется одна и та же переменная bookinfo, данные по сути ни где не фиксируется в промежутке, не хорошо
+        if bookInfo:
+            storeBooks(bookInfo)
 
 
-
-
+def main():
+    #Используем landing page сайта, чтобы получить список жанров, которые есть на сайте, для формирование ссылок на страницу каждого жанра
+    landingPageLink = "https://books.toscrape.com/index.html"
+    
 
 
 
